e957c32cd7cc19daa43b16456a992b1aa8cca5b6
zzz-tang <zziyun@email.unc.edu>
2021-11-24 10:11:30 -0500

    Set up database table and API endpoints for users.

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-////////////////////////////// SETUP
+//////////////////////////////////////////////////////////// SETUP
 
 // Enable strict mode
 // Ensures that things will not fail silently and that errors will be thrown
@@ -10,6 +10,38 @@ const Database = require( 'better-sqlite3' );
 // Connect to database
 // If nonexistent then create database
 const db = new Database( 'data.db' );
+
+//////////////////////////////////////////////////////////// USER TABLE
+
+// Determine whether user table has been initialized
+// If uninitialized then initialize table
+
+let userInfoExists = db.prepare(
+  `SELECT name
+  FROM sqlite_master
+  WHERE type='table' and name='userInfo';`
+).get();
+
+if ( userInfoExists === undefined ) {
+
+  console.log( 'Did not find table for user information, will create' );
+  
+  // Create table for user information
+  db.exec(
+    `CREATE TABLE userInfo (
+      userId INTEGER PRIMARY KEY,
+      username TEXT NOT NULL UNIQUE, /* CHECK cannot be empty string */
+      pass TEXT NOT NULL /* CHECK cannot be empty string */
+    );`
+  );
+
+  console.log( 'Created table for user information, table currently empty' );
+
+} else {
+
+  console.log( 'There exists a table for user information' );
+
+}
 
 //////////////////////////////////////////////////////////// EXPORT
 
--- server.js
+++ server.js
@@ -28,6 +28,62 @@ app.get( "/app", ( req, res ) => {
   res.status(200).json( { "message": "API works"} );
 } );
 
+//////////////////////////////////////////////////////////// API FOR USERS
+
+// CREATE new user (HTTP method POST)
+// At endpoint /app/new/user/
+app.post( "/app/new/user", ( req, res ) => {
+  const stmt = db.prepare(
+    `INSERT INTO userInfo ( username, pass )
+    VALUES ( ?, ? );`
+  );
+  const info = stmt.run( req.body.username, md5( req.body.pass ) );
+  res.status(201).json( { "message" : info.changes + " record created: ID " + info.lastInsertRowid + " (201)" } );
+} );
+
+// READ list of all users (HTTP method GET)
+// At endpoint /app/users/
+app.get( "/app/users", ( req, res ) => {
+  const stmt = db.prepare(
+    `SELECT * FROM userInfo`
+  );
+  const userList = stmt.all();
+  //res.status(200).json( userList );
+  res.status(200).send( JSON.stringify( userList, null, "\t" ) );
+} );
+
+// READ a single user (HTTP method GET)
+// At endpoint /app/user/
+app.get( "/app/user", ( req, res ) => {
+  const stmt = db.prepare(
+    `SELECT * FROM userInfo WHERE userId = ?`
+  );
+  const user = stmt.get( req.body.userId );
+  res.status(200).send( JSON.stringify( user, null, "\t" ) );
+} );
+
+// UPDATE a single user (HTTP method PATCH)
+// At endpoint /app/update/user
+app.patch( "/app/update/user", ( req, res ) => {
+  const stmt = db.prepare(
+    `UPDATE userInfo
+    SET username = COALESCE( ?, username ), pass = COALESCE( ?, pass )
+    WHERE userId = ?`
+  );
+  const info = stmt.run( req.body.username, md5(req.body.pass), req.body.userId );
+  res.status(200).json( { "message" : info.changes + " record updated: ID " + req.body.userId + " (200)" } );
+} );
+
+// DELETE a single user (HTTP method DELETE)
+// At endpoint /app/delete/user
+app.delete( "/app/delete/user", (req, res) => {
+  const stmt = db.prepare(
+    `DELETE FROM userInfo WHERE userId = ?`
+  );
+  const info = stmt.run( req.body.userId );
+  res.status(200).json( { "message" : info.changes + " record deleted: ID " + req.body.userId + " (200)" } );
+} );
+
 //////////////////////////////////////////////////////////// DEFAULT RESPONSE
 
 app.use( function( req, res ){
